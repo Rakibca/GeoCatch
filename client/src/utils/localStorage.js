7f7caa8cf6781182dd89bd8bae9088cc8ad3aa2e
nathanh635 <nathanh635@gmail.com>
2022-07-29 10:19:46 -0600

    got map working

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,30 +1,30 @@
-export const getSavedImageIds = () => {
-  const savedImageIds = localStorage.getItem('saved_images')
-    ? JSON.parse(localStorage.getItem('saved_images'))
-    : [];
+// export const getSavedImageIds = () => {
+//   const savedImageIds = localStorage.getItem('saved_images')
+//     ? JSON.parse(localStorage.getItem('saved_images'))
+//     : [];
 
-  return savedImageIds;
-};
+//   return savedImageIds;
+// };
 
-export const saveImagesIds = (imageIdArr) => {
-  if (imageIdArr.length) {
-    localStorage.setItem('saved_images', JSON.stringify(bookIdArr));
-  } else {
-    localStorage.removeItem('saved_images');
-  }
-};
+// export const saveImagesIds = (imageIdArr) => {
+//   if (imageIdArr.length) {
+//     localStorage.setItem('saved_images', JSON.stringify(bookIdArr));
+//   } else {
+//     localStorage.removeItem('saved_images');
+//   }
+// };
 
-export const removeImageId = (bookId) => {
-  const savedImageIds = localStorage.getItem('saved_images')
-    ? JSON.parse(localStorage.getItem('saved_images'))
-    : null;
+// export const removeImageId = (bookId) => {
+//   const savedImageIds = localStorage.getItem('saved_images')
+//     ? JSON.parse(localStorage.getItem('saved_images'))
+//     : null;
 
-  if (!savedImageIds) {
-    return false;
-  }
+//   if (!savedImageIds) {
+//     return false;
+//   }
 
-  const updatedSavedImageIds = savedImageIds?.filter((savedImageId) => savedImageId !== ImageId);
-  localStorage.setItem('saved_images', JSON.stringify(updatedSavedImageIds));
+//   const updatedSavedImageIds = savedImageIds?.filter((savedImageId) => savedImageId !== ImageId);
+//   localStorage.setItem('saved_images', JSON.stringify(updatedSavedImageIds));
 
-  return true;
-};
+//   return true;
+// };
